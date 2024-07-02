7dc7266f0e5f75f991fe71a0d97c71f29920bece
RoySCV <roys@codevalue.com>
2024-07-02 09:08:33 +0300

    efficiency plugin pipeline

--- pipeline/plugin-python-webapp-pipeline.groovy
+++ pipeline/plugin-python-webapp-pipeline.groovy
@@ -5,30 +5,32 @@ pipeline {
         dockerTag = "latest"
     }
     stages {
-        stage('Docker Build') {
+        stage('Setup') {
             steps {
                 script {
                     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKERHUB_USR', passwordVariable: 'DOCKERHUB_PSW')]) {
-                        def dockerImage = "${DOCKERHUB_USR}/web-app"
-                        def dockerImageFullName = "${dockerImage}:${dockerTag}"
-                        echo "Docker Build: \n  File: ${dockerfilePath}\n  Image: ${dockerImageFullName}\n  Context: ${WORKSPACE}"
-                        // Using Docker plugin to build the image
-                        docker.build(dockerImageFullName, "-f ${dockerfilePath} ${WORKSPACE}")
+                        env.DOCKER_IMAGE = "${DOCKERHUB_USR}/web-app"
+                        env.DOCKER_IMAGE_FULL_NAME = "${env.DOCKER_IMAGE}:${dockerTag}"
                     }
+                }
+            }
+        }
+        stage('Docker Build') {
+            steps {
+                script {
+                    echo "Docker Build: \n  File: ${dockerfilePath}\n  Image: ${env.DOCKER_IMAGE_FULL_NAME}\n  Context: ${WORKSPACE}"
+                    // Using Docker plugin to build the image
+                    docker.build(env.DOCKER_IMAGE_FULL_NAME, "-f ${dockerfilePath} ${WORKSPACE}")
                 }
             }
         }
         stage('Docker Push') {
             steps {
                 script {
-                    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKERHUB_USR', passwordVariable: 'DOCKERHUB_PSW')]) {
-                        def dockerImage = "${DOCKERHUB_USR}/web-app"
-                        def dockerImageFullName = "${dockerImage}:${dockerTag}"
-                        echo '----------- Pushing to Docker Hub -----------'
-                        // Using Docker plugin to push the image with automatic login
-                        docker.withRegistry('https://index.docker.io/v1/', 'dockerhub-credentials') {
-                            docker.image(dockerImageFullName).push(dockerTag)
-                        }
+                    echo '----------- Pushing to Docker Hub -----------'
+                    // Using Docker plugin to push the image with automatic login
+                    docker.withRegistry('https://index.docker.io/v1/', 'dockerhub-credentials') {
+                        docker.image(env.DOCKER_IMAGE_FULL_NAME).push(dockerTag)
                     }
                 }
             }
