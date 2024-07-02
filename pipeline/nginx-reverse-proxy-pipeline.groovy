6b02c3f955b1c6c8a289367217aa272784ef3803
RoySCV <roys@codevalue.com>
2024-07-02 09:32:26 +0300

    update nginx-reverse-proxy

--- pipeline/nginx-reverse-proxy-pipeline.groovy
+++ pipeline/nginx-reverse-proxy-pipeline.groovy
@@ -2,32 +2,35 @@ pipeline {
     agent any
     environment {
         dockerfilePath = "nginx-reverse-proxy/Dockerfile"
-        dockerTag = "latest"
     }
     stages {
-        stage('Docker Build') {
+        stage('Setup') {
             steps {
                 script {
                     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKERHUB_USR', passwordVariable: 'DOCKERHUB_PSW')]) {
-                        def dockerImage = "${DOCKERHUB_USR}/nginx-reverse-proxy"
-                        def dockerImageFullName = "${dockerImage}:${dockerTag}"
-                        echo '----------- Docker Build -----------'
-                        echo "Docker Build: \n  File: ${dockerfilePath}\n  Image: ${dockerImageFullName}\n  Context: ${WORKSPACE}"
-                        sh "docker build -f ${dockerfilePath} -t ${dockerImageFullName} ."
+                        env.DOCKER_IMAGE = "${DOCKERHUB_USR}/nginx-reverse-proxy"
+                        env.DOCKER_TAG = "build-${env.BUILD_NUMBER}"
+                        env.DOCKER_IMAGE_FULL_NAME = "${env.DOCKER_IMAGE}:${env.DOCKER_TAG}"
                     }
+                }
+            }
+        }
+        stage('Docker Build') {
+            steps {
+                script {
+                    echo "Docker Build: \n  File: ${dockerfilePath}\n  Image: ${env.DOCKER_IMAGE_FULL_NAME}\n  Context: ${WORKSPACE}"
+                    // Using Docker plugin to build the image
+                    docker.build(env.DOCKER_IMAGE_FULL_NAME, "-f ${dockerfilePath} ${WORKSPACE}")
                 }
             }
         }
         stage('Docker Push') {
             steps {
                 script {
-                    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKERHUB_USR', passwordVariable: 'DOCKERHUB_PSW')]) {
-                        def dockerImage = "${DOCKERHUB_USR}/nginx-reverse-proxy"
-                        def dockerImageFullName = "${dockerImage}:${dockerTag}"
-                        echo '----------- Login Docker Hub -----------'
-                        sh "echo ${DOCKERHUB_PSW} | docker login -u ${DOCKERHUB_USR} --password-stdin"
-                        echo '----------- Pushing to Docker Hub -----------'
-                        sh "docker push ${dockerImageFullName}"
+                    echo '----------- Pushing to Docker Hub -----------'
+                    // Using Docker plugin to push the image with automatic login
+                    docker.withRegistry('https://index.docker.io/v1/', 'dockerhub-credentials') {
+                        docker.image(env.DOCKER_IMAGE_FULL_NAME).push(env.DOCKER_TAG)
                     }
                 }
             }
