5de85b60d9ff535939ede72042a05f32027a9c0c
Roy Solomon <48485109+Roysm@users.noreply.github.com>
2024-07-02 07:40:41 +0300

    Update hello-world.groovy

--- pipeline/tests/hello-world.groovy
+++ pipeline/tests/hello-world.groovy
@@ -1,6 +1,12 @@
 pipeline {
     agent any
-
+    environment {
+        dockerhub = credentials('dockerhub')
+        dockerfilePath = "webapp/Dockerfile"
+        dockerImage = "${dockerhub_USR}/web-app"
+        dockerTag = "latest"
+        dockerImageFullName = "${dockerImage}:${dockerTag}"
+    }
     stages {
         stage('Hello') {
             steps {
@@ -10,4 +16,24 @@ pipeline {
             }
         }
     }
+    
+    stage('Docker Build') {
+            steps {
+                script {
+                    echo "Docker Build: \n  File: ${dockerfilePath}\n  Image: ${dockerImageFullName}\n  Context: ${WORKSPACE}"
+                    sh "docker build -f ${dockerfilePath} -t ${dockerImageFullName} ."
+                }
+            }
+        }
+    
+    stage('Docker Push') {
+            steps {
+                script {
+                    echo '----------- Login Docker Hub -----------'
+                    sh "echo ${dockerhub_PSW} | docker login -u ${dockerhub_USR} --password-stdin"
+                    echo '----------- Pushing to Docker Hub -----------'
+                    sh "docker push ${dockerImageFullName}"
+                }
+            }
+        }
 }
