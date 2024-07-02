de18f734c1eb1212c9a56b1c298e2f6ed1496d03
Roy Solomon <48485109+Roysm@users.noreply.github.com>
2024-07-02 08:11:05 +0300

    Update hello-world.groovy

--- pipeline/tests/hello-world.groovy
+++ pipeline/tests/hello-world.groovy
@@ -2,9 +2,7 @@ pipeline {
     agent any
     environment {
         dockerfilePath = "webapp/Dockerfile"
-        dockerImage = "${DOCKERHUB_USR}/web-app"
         dockerTag = "latest"
-        dockerImageFullName = "${dockerImage}:${dockerTag}"
     }
     stages {
         stage('Hello') {
@@ -18,8 +16,13 @@ pipeline {
         stage('Docker Build') {
             steps {
                 script {
-                    echo "Docker Build: \n  File: ${dockerfilePath}\n  Image: ${dockerImageFullName}\n  Context: ${WORKSPACE}"
-                    sh "docker build -f ${dockerfilePath} -t ${dockerImageFullName} ."
+                    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKERHUB_USR', passwordVariable: 'DOCKERHUB_PSW')]) {
+                        def dockerImage = "${DOCKERHUB_USR}/web-app"
+                        def dockerImageFullName = "${dockerImage}:${dockerTag}"
+                        echo '----------- Docker Build -----------'
+                        echo "Docker Build: \n  File: ${dockerfilePath}\n  Image: ${dockerImageFullName}\n  Context: ${WORKSPACE}"
+                        sh "docker build -f ${dockerfilePath} -t ${dockerImageFullName} ."
+                    }
                 }
             }
         }
@@ -27,11 +30,13 @@ pipeline {
         stage('Docker Push') {
             steps {
                 script {
-                    echo '----------- Login Docker Hub -----------'
                     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKERHUB_USR', passwordVariable: 'DOCKERHUB_PSW')]) {
-                    sh "echo ${DOCKERHUB_PSW} | docker login -u ${DOCKERHUB_USR} --password-stdin"
-                    echo '----------- Pushing to Docker Hub -----------'
-                    sh "docker push ${dockerImageFullName}"
+                        def dockerImage = "${DOCKERHUB_USR}/web-app"
+                        def dockerImageFullName = "${dockerImage}:${dockerTag}"
+                        echo '----------- Login Docker Hub -----------'
+                        sh "echo ${DOCKERHUB_PSW} | docker login -u ${DOCKERHUB_USR} --password-stdin"
+                        echo '----------- Pushing to Docker Hub -----------'
+                        sh "docker push ${dockerImageFullName}"
                     }
                 }
             }
