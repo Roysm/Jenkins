e4fe45a3a07b1e3e83fc25d54dc93052411727aa
RoySCV <roys@codevalue.com>
2024-07-02 09:30:59 +0300

    adding nginx-reverse-proxy

---
+++ pipeline/nginx-reverse-proxy-pipeline.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent any
+    environment {
+        dockerfilePath = "nginx-reverse-proxy/Dockerfile"
+        dockerTag = "latest"
+    }
+    stages {
+        stage('Docker Build') {
+            steps {
+                script {
+                    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKERHUB_USR', passwordVariable: 'DOCKERHUB_PSW')]) {
+                        def dockerImage = "${DOCKERHUB_USR}/nginx-reverse-proxy"
+                        def dockerImageFullName = "${dockerImage}:${dockerTag}"
+                        echo '----------- Docker Build -----------'
+                        echo "Docker Build: \n  File: ${dockerfilePath}\n  Image: ${dockerImageFullName}\n  Context: ${WORKSPACE}"
+                        sh "docker build -f ${dockerfilePath} -t ${dockerImageFullName} ."
+                    }
+                }
+            }
+        }
+        stage('Docker Push') {
+            steps {
+                script {
+                    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKERHUB_USR', passwordVariable: 'DOCKERHUB_PSW')]) {
+                        def dockerImage = "${DOCKERHUB_USR}/nginx-reverse-proxy"
+                        def dockerImageFullName = "${dockerImage}:${dockerTag}"
+                        echo '----------- Login Docker Hub -----------'
+                        sh "echo ${DOCKERHUB_PSW} | docker login -u ${DOCKERHUB_USR} --password-stdin"
+                        echo '----------- Pushing to Docker Hub -----------'
+                        sh "docker push ${dockerImageFullName}"
+                    }
+                }
+            }
+        }
+    }
+}
